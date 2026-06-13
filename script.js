89d35632b563100f95fcf6bd29ea27c79faa9c9d
HenriqueGS12 <hgs12conta@gmail.com>
2026-06-13 12:13:43 -0300

    entrada de texto e contagem de caracteres, e teste de funcionamento

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const caixaTexto = document.getElementById('caixa-texto');
+const contadorGeral = document.getElementById('contador-geral');
+
 function formatarParaNegritoUnicode(texto) {
     const de = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const para = "𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗";
@@ -10,5 +13,12 @@ function formatarParaNegritoUnicode(texto) {
     ).join('');
 }
 
-console.log("Olá, mundo! O JavaScript está rodando perfeitamente.");
-console.log(formatarParaNegritoUnicode("Teste 123"));
+caixaTexto.addEventListener('input', () => 
+    {
+        const textoAtual = caixaTexto.value;
+
+        const totalCaracteres = textoAtual.length;
+
+        contadorGeral.textContent = totalCaracteres;
+    }
+);
