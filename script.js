2d57005a2793a5c9e0d46acda536b92d0967874c
HenriqueGS12 <hgs12conta@gmail.com>
2026-06-13 13:16:11 -0300

    função de saída e copia

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const caixaTexto = document.getElementById('caixa-texto');
 const contadorGeral = document.getElementById('contador-geral');
 
+const textoSaida = document.getElementById('texto-saida');
+const botaoCopiar = document.getElementById('botao-copiar');
+
 function formatarParaNegritoUnicode(texto) {
     const de = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const para = "𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗";
@@ -16,9 +19,26 @@ function formatarParaNegritoUnicode(texto) {
 caixaTexto.addEventListener('input', () => 
     {
         const textoAtual = caixaTexto.value;
+        contadorGeral.textContent = textoAtual.length;
+        const textoFormatado = formatarParaNegritoUnicode(textoAtual);
+        textoSaida.textContent = textoFormatado;
+    }
+);
 
-        const totalCaracteres = textoAtual.length;
+botaoCopiar.addEventListener('click', () =>
+    {
+        if (!textoSaida.textContent) return;
+        
+        const textoParaCopiar = textoSaida.textContent;
 
-        contadorGeral.textContent = totalCaracteres;
+        navigator.clipboard.writeText(textoParaCopiar)
+            .then(() =>
+                {
+                    const textoOriginal = botaoCopiar.textContent;
+                    botaoCopiar.textContent = "Copiado";
+                    setTimeout(() => botaoCopiar.textContent = textoOriginal, 1000);
+                }
+            )
+            .catch(erro => console.error("Erro ao copiar texto: ", erro));
     }
 );
